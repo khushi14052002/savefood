2292377f10842ee1747fc6e7fdfdf835fb1ea0c9
khushi14052002 <67247338+khushi14052002@users.noreply.github.com>
2021-10-31 09:00:11 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,80 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What kind of food we waste the most?",
+    answer: "Fruit,vegetables",
+    options: [
+      "Fruit,vegetables",
+      "Sweets",
+      "Dairy products",
+      "Herbs and spices"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In what ways we can waste less food?",
+    answer: "We should think wisely before meal if we are really capable for eating it.",
+    options: [
+      "We should buy food, which we don't like eating e.g. broccoli.",
+      "We should drink more water instead of eating meals.",
+      "We should think wisely before meal if we are really capable for eating it.",
+      "We should buy a bigger fridge."
+    ]
+  },
+    {
+    numb: 3,
+    question: "What can we do with leftovers after lunch?",
+    answer: "We can reuse them in another dish",
+    options: [
+      "We can reuse them in another dish",
+      "We can put it to cupboard for another few days until we want to eat it again",
+      "We can give them to kids to play with and have fun",
+      "We should throw them away because everyone is full after meal"
+    ]
+  },
+    {
+    numb: 4,
+    question: " What you shouldn't do while shopping?",
+    answer: "You must buy a lot of products on special sales only.",
+    options: [
+      "Check what you have at home before you shop.",
+      "You must buy a lot of products on special sales only.",
+      "Make a list of products you want to buy to prevent from buying unneeded things.",
+      "Check the expiry dates on products which you want to buy."
+    ]
+  },
+  {
+    numb: 5,
+    question: "What can you do to help prevent food waste?",
+    answer: "All of the above",
+    options: [
+      "Don't buy more food than you can cook.",
+      "Eat leftovers on a regular night each week.",
+      "Shop at stores that offer misshapen food at a discount.",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who can teach others about food waste.",
+    answer: "Each of us",
+    options: [
+      "Only teachers",
+      "Only parents",
+      "Each of us",
+      "Doctors"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Items that can be composted include",
+    answer: "All of the above",
+    options: [
+      "Tea leaves and coffee grinds",
+      "Vegetable peelings",
+      "Pizza boxes",
+      "All of the above"
+    ]
+  },
+];
---
+++ script.js
@@ -0,0 +1,81 @@
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+
+
+////////Slider
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+//feedback
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <h5>We'll use your feedback to improve our customer support</h5>
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
